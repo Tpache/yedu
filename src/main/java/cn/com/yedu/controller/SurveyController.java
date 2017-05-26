b4eae8b1ae36c9f3f8770c10dada69e0ba4fe412
mengpf <mengpf_057@163.com>
2017-05-26 14:05:54 +0800

    问卷调查页面新增
    Signed-off-by: mengpf <mengpf_057@163.com>

--- src/main/java/cn/com/yedu/controller/SurveyController.java
+++ src/main/java/cn/com/yedu/controller/SurveyController.java
@@ -1,7 +1,12 @@
 package cn.com.yedu.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Administrator
@@ -10,6 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/surveyContr")
 public class SurveyController {
+	
+	private static Logger logger = Logger.getLogger(SurveyController.class); 
+	
+	@RequestMapping(value = "/addSurvey")
+	public ModelAndView addSurvey(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		logger.info("用户访问HomeController.class----->  /surveyContr/addSurvey.html");
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("web/addSurvey");
+		return modelAndView;
+	} 
 	
 	private boolean isTableExists(String flag){
 		
